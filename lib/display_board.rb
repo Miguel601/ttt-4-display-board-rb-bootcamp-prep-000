dde4f7f56d827270b8f97e75ed98f33f1062992a
Miguel Henriquez <henriquez.miguel@outlook.com>
2018-11-24 01:22:17 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,17 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+def display_board
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+  puts "-----------"
+  puts "   |   |   "
+end
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+def display_board(board)
+end
+
+puts display_board(board#{0})
